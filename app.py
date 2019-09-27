3c11e85f43878c208cc0ca19e4199c1c834403a0
Erin Partlan <erpartlan@gmail.com>
2019-09-27 01:18:33 -0400

    New dash webapp showing premiums estimator.

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
+import numpy as np
 import pandas as pd
 import pickle
+from patsy import dmatrices
+import statsmodels.api as sm
+import geopy
+from geopy.geocoders import Nominatim
 
-filename = 'MA1.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
 
-coords = pd.read_csv("coords.csv")
-obs_count = pd.read_csv('obs_count')
+#IMPORT MODELS
+filename = 'model/Poisson2reg.sav'
+count_model = pickle.load(open(filename, 'rb'))
 
-zhat_all = pd.Series([])
-for i in range(len(coords.coords)):
-	data = obs_count[coords.coords[i]][:-5]
-	if data.sum() == 0:
-		zhat = pd.DataFrame(pd.Series([0,0,0,0,0,0,0,0]).values.reshape(1,-1))
-		zhat_all = pd.concat([zhat_all, zhat])
-	else:
-		zhat = loaded_model.predict(len(data), len(data)+7)
-		zhat = pd.DataFrame(zhat.values.reshape(1,-1))
-		zhat_all = pd.concat([zhat_all, zhat]) 
+filename = 'model/rfClaimFrac.sav'
+value_model = pickle.load(open(filename, 'rb'))
+
+
+# IMPORT NON-USER INPUT DATA
+coord_lookup = pd.read_csv('data/coord_lookup.csv') #contains coordinate [coords], distance [distance], and future housing density [future_density] info
 
 
 
@@ -32,58 +32,98 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div(children=[
-	html.H1(children='Salty Living: Coastal housing under threat'),
-
-	html.Div(children='An interactive tool for understanding extents of flood damage in Florida and effectiveness of preventative measures.'),
-
-	# dcc.Input(
-	# 	placeholder='',
-	# 	id='address',
-	# 	type='text',
-	# 	value=''
-	# ),
-
-	dcc.Graph(
-		id='example-graph',
-		figure=go.Figure(
-			data=go.Scatter(x=coords["longitude"].values, y=coords["latitude"].values, mode='markers'),
-			layout=go.Layout(dict(
-				height=600,
-				margin={'l': 0, 'b': 0, 't': 0, 'r': 0}
-			))
-		),
+	html.H1(children='Surge Protector',
 		style={
-			'width': '35%',
-			'display': 'inline-block',
-			'vertical-align': 'middle'
-		}
-	),
+            'textAlign': 'center',
+        }),
+    html.H2(children='Make smarter home buying decisions',
+    	style={
+            'textAlign': 'center',
+        }),
+    html.Div(children="Uses the likelihood of future flood damage to estimate insurance premiums for homes in Florida.",
+  		style={
+            'textAlign': 'center',
+        }),
 
-	dcc.Dropdown(
-		id='address',
-		options=[
-			{'label': '2019', 'value': 5},
-			{'label': '2020', 'value': 6},
-			{'label': '2021', 'value': 7}
-		],
-		value=5
-	),  
 
-	html.Div(
-		children=coords["longitude"].values[0],
-		id='model-values',
-		style={
-			'width': '35%',
-			'display': 'inline-block',
-			'vertical-align': 'middle'
-		}
-	),
-])
+    html.Div(className="row", children=[
+        html.Div(className="six columns", 
+            children=['Location: ',
+            dcc.Input(
+		     	placeholder='Enter address, etc.',
+		     	id='address',
+		     	type='text',
+		     	value=''
+	    	),
+	    	'Home Value: ',
+	    	dcc.Input(
+		     	placeholder='Enter home value',
+		     	id='home_value',
+		     	type='text',
+		     	value=''
+	    	)],
+        ),
+        html.Div(className="one column",children='Min. Coverage '),
+        html.Div(className="four columns", children=[
+        	dcc.Slider(
+                id='exp_value',
+                min=0,
+                max=500,
+                step=50,
+                marks={
+                    0: 'High Risk',
+                    500: 'Low Risk',
+                },
+                value=250,
+            ),
+            html.Div(id='slider-output-container'),
+        ]),
+        html.Div(className="one column",children='Excess Coverage'),
+                
+        html.Div(
+			children='',
+			id='premium'
+			),
+		]),
+    ])
 
-@app.callback(Output('model-values', 'children'),
-			  [Input('address', 'value')])
-def update_metrics(n):
-	return zhat[n].mean()
+
+@app.callback(Output('premium', 'children'),
+              [Input('address', 'value'),
+               Input('exp_value', 'value'),
+               Input('home_value', 'value')])
+def update_metrics(address, exp_value, home_value):
+	geolocator = Nominatim(user_agent="surge-protector")
+	location = geolocator.geocode(address)
+	if location == None or home_value == "":
+		return ''
+	lat_ = round(location.latitude,1)
+	long_ = round(location.longitude,1)
+	coord_input =  str(lat_)+', '+str(long_)
+	dt = coord_lookup[coord_lookup.coords == coord_input].distance.values[0] #calculated from input location (NN coords or active calc to polygon)
+	ds = coord_lookup[coord_lookup.coords == coord_input].future_density.values[0] # lookup from input location (NN coords
+	# RUN MODELS
+	intercept = 1
+	count_test = np.array([intercept,dt,ds])
+	count_preds = count_model.get_prediction(count_test)
+	count_func = count_preds.summary_frame()
+	count = count_func['mean'] # first column is mean count (2 is std err, 3 is ci lower, 4 is ci upper)
+	prob = count/(dt*123876900) # 123876900 square meters in a square with side 0.1 degrees
+
+	ct = count # predicted by Poisson2reg
+	xtest = np.array([dt,ct,ds])
+	frac =np.exp(value_model.predict(xtest.reshape(1, -1)))
+
+	premium = (float(exp_value) - prob*frac*float(home_value))/(1 - prob)
+
+	return 'Monthly Premium = ${:0.2f}'.format(round(premium.values[0],2))
+	# return "$"+home_value+"$"
+
+@app.callback(
+    dash.dependencies.Output('slider-output-container', 'children'),
+    [dash.dependencies.Input('exp_value', 'value')])
+def update_output(exp_value):
+    return #'You have selected "{}"'.format(exp_value)
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
